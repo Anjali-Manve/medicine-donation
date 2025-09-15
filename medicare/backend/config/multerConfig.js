56a8c5c0d6f6716c0441aa9d01cc37584ab04a30
anijali Manve <anjalimanve2828@gmail.com>
2025-09-15 15:45:38 +0530

    add multer

--- medicare/backend/config/multerConfig.js
+++ medicare/backend/config/multerConfig.js
@@ -1,49 +1,4 @@
-// import multer from 'multer';
-// import path from 'path';
-// import fs from 'fs';
 
-// // Define the destination folder for uploads
-// const uploadDir = path.join(process.cwd(), 'uploads', 'profile-pictures');
-
-// // Ensure the upload directory exists
-// const ensureDirectoryExistence = (dir) => {
-//   if (!fs.existsSync(dir)) {
-//     fs.mkdirSync(dir, { recursive: true });
-//   }
-// };
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     ensureDirectoryExistence(uploadDir);
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     // Generate a unique filename: user ID + timestamp + original file extension
-//     // Assuming req.user is set by the protect middleware and contains req.user.id
-//     const userId = req.user && req.user.id ? req.user.id : 'unknown-user';
-//     cb(null, userId + '-' + Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   const filetypes = /jpeg|jpg|png|gif/;
-//   const mimetype = filetypes.test(file.mimetype);
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//   if (mimetype && extname) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error('Images Only! Allowed types: JPEG, JPG, PNG, GIF'), false);
-//   }
-// };
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 1024 * 1024 * 5 }, // 5MB file size limit
-//   fileFilter: fileFilter,
-// });
-
-// export default upload;
 import multer from "multer";
 import path from "path";
 import fs from "fs";
